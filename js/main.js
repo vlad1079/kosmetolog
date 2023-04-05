277137bcc12c448edf734187721d90f9b8f2e83a
vlad1010 <misha1079@ukr.net>
2023-04-05 23:06:45 +0300

    05_04_2023

--- js/main.js
+++ js/main.js
@@ -3,10 +3,12 @@ if (iconMenu) {
     const iconBody = document.querySelector('.header_container');
 
     const logo = document.querySelector('.header_top_logo');
+    const headerContainerTop = document.querySelector('.header_container_top');
     iconMenu.addEventListener('click' , function  (e) {
         iconMenu.classList.toggle('active');
         iconBody.classList.toggle('active');
         logo.classList.toggle('active');
+        headerContainerTop.classList.toggle('active');
         document.body.classList.toggle('lock');
     });
 }
